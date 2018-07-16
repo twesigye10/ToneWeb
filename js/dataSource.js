f854e0a4d50765fdcba14dd5f8ec679d673f429a
twesigye10 <twesigye10@gmail.com>
2018-07-16 19:09:48 +0300

    Adding existing files

---
+++ js/dataSource.js
@@ -0,0 +1,128 @@
+var people = [{"name":"Marilyn","surname":"Turner","gender":"Female","email":"mturner0@goodreads.com","city":"Driyorejo","country":"Indonesia"},
+{"name":"Kenneth","surname":"Gibson","gender":"Male","email":"kgibson1@reference.com","city":"Khāsh","country":"Afghanistan"},
+{"name":"Brenda","surname":"Reyes","gender":"Female","email":"breyes2@icio.us","city":"Yangjian","country":"China"},
+{"name":"Frances","surname":"Dixon","gender":"Female","email":"fdixon3@ftc.gov","postalAddress":"8310","city":"Dinaig","country":"Philippines"},
+{"name":"Phillip","surname":"Sims","gender":"Male","email":"psims4@dagondesign.com","postalAddress":"554 52","city":"Jönköping","country":"Sweden"},
+{"name":"Helen","surname":"Allen","gender":"Female","email":"hallen5@wordpress.com","postalAddress":"31110","city":"Ubonratana","country":"Thailand"},
+{"name":"Heather","surname":"Garcia","gender":"Female","email":"hgarcia6@flickr.com","postalAddress":"353993","city":"Novorossiysk","country":"Russia"},
+{"name":"Brian","surname":"Olson","gender":"Male","email":"bolson7@biblegateway.com","city":"Karang","country":"Indonesia"},
+{"name":"Deborah","surname":"Sanders","gender":"Female","email":"dsanders8@sitemeter.com","city":"Qinglin","country":"China"},
+{"name":"Robin","surname":"Burke","gender":"Female","email":"rburke9@github.com","postalAddress":"18650-000","city":"São Manuel","country":"Brazil"},
+{"name":"Jimmy","surname":"Mitchell","gender":"Male","email":"jmitchella@theatlantic.com","postalAddress":"937 91","city":"Burträsk","country":"Sweden"},
+{"name":"Amy","surname":"Harvey","gender":"Female","email":"aharveyb@webs.com","city":"Mmaaf","country":"Indonesia"},
+{"name":"Joe","surname":"Porter","gender":"Male","email":"jporterc@walmart.com","postalAddress":"2806","city":"San Felipe Old","country":"Philippines"},
+{"name":"Stephen","surname":"Reyes","gender":"Male","email":"sreyesd@so-net.ne.jp","postalAddress":"6315","city":"Ubay","country":"Philippines"},
+{"name":"Charles","surname":"Reynolds","gender":"Male","email":"creynoldse@newyorker.com","city":"Semambung","country":"Indonesia"},
+{"name":"Joshua","surname":"Murray","gender":"Male","email":"jmurrayf@hp.com","city":"Pisan","country":"Indonesia"},
+{"name":"Peter","surname":"Lopez","gender":"Male","email":"plopezg@friendfeed.com","postalAddress":"763 26","city":"Luhačovice","country":"Czech Republic"},
+{"name":"Debra","surname":"Boyd","gender":"Female","email":"dboydh@hatena.ne.jp","city":"Shuinan","country":"China"},
+{"name":"Eric","surname":"Brooks","gender":"Male","email":"ebrooksi@liveinternet.ru","city":"Curug","country":"Indonesia"},
+{"name":"Shawn","surname":"Reyes","gender":"Male","email":"sreyesj@cyberchimps.com","city":"Tabant","country":"Morocco"},
+{"name":"Todd","surname":"Jordan","gender":"Male","email":"tjordank@youku.com","city":"Nosy Varika","country":"Madagascar"},
+{"name":"Jose","surname":"Dean","gender":"Male","email":"jdeanl@ihg.com","postalAddress":"3801","city":"Balbalan","country":"Philippines"},
+{"name":"Andrew","surname":"Fowler","gender":"Male","email":"afowlerm@51.la","postalAddress":"747 84","city":"Hradec nad Moravici","country":"Czech Republic"},
+{"name":"Beverly","surname":"Brooks","gender":"Female","email":"bbrooksn@quantcast.com","city":"Ampanihy","country":"Madagascar"},
+{"name":"Edward","surname":"Frazier","gender":"Male","email":"efraziero@va.gov","postalAddress":"4410","city":"Ragay","country":"Philippines"},
+{"name":"Linda","surname":"Pierce","gender":"Female","email":"lpiercep@blogs.com","postalAddress":"157008","city":"Buy","country":"Russia"},
+{"name":"Mark","surname":"Wallace","gender":"Male","email":"mwallaceq@berkeley.edu","city":"Pengandonan","country":"Indonesia"},
+{"name":"Thomas","surname":"Owens","gender":"Male","email":"towensr@naver.com","postalAddress":"30430","city":"Sida","country":"Thailand"},
+{"name":"Christina","surname":"Romero","gender":"Female","email":"cromeros@amazon.com","postalAddress":"439 07","city":"Peruc","country":"Czech Republic"},
+{"name":"Joseph","surname":"Weaver","gender":"Male","email":"jweavert@indiegogo.com","city":"Mamponteng","country":"Ghana"},
+{"name":"Jesse","surname":"Hudson","gender":"Male","email":"jhudsonu@google.co.uk","city":"Wuxi","country":"China"},
+{"name":"Kelly","surname":"Day","gender":"Female","email":"kdayv@360.cn","city":"Ambatolaona","country":"Madagascar"},
+{"name":"Gerald","surname":"Stone","gender":"Male","email":"gstonew@t-online.de","postalAddress":"4415-092","city":"Pedroso","country":"Portugal"},
+{"name":"Jose","surname":"Nichols","gender":"Male","email":"jnicholsx@nymag.com","city":"Lanyi","country":"China"},
+{"name":"Larry","surname":"Lee","gender":"Male","email":"lleey@sphinn.com","city":"Orguz","country":"Bosnia and Herzegovina"},
+{"name":"Bobby","surname":"Fisher","gender":"Male","email":"bfisherz@answers.com","postalAddress":"601247","city":"Chuchkovo","country":"Russia"},
+{"name":"Roy","surname":"Ross","gender":"Male","email":"rross10@so-net.ne.jp","city":"Burgastai","country":"China"},
+{"name":"Angela","surname":"Henry","gender":"Female","email":"ahenry11@sciencedirect.com","city":"Sedandang","country":"Indonesia"},
+{"name":"Donald","surname":"Hansen","gender":"Male","email":"dhansen12@seesaa.net","city":"Dongtou","country":"China"},
+{"name":"Richard","surname":"Walker","gender":"Male","email":"rwalker13@bing.com","city":"Gerdu","country":"Indonesia"},
+{"name":"Donald","surname":"Montgomery","gender":"Male","email":"dmontgomery14@sohu.com","city":"Chengguan","country":"China"},
+{"name":"George","surname":"Myers","gender":"Male","email":"gmyers15@skyrock.com","city":"Apatin","country":"Serbia"},
+{"name":"Albert","surname":"Mason","gender":"Male","email":"amason16@t.co","postalAddress":"39029 CEDEX","city":"Lons-le-Saunier","country":"France"},
+{"name":"Chris","surname":"Hawkins","gender":"Male","email":"chawkins17@hexun.com","postalAddress":"31901","city":"Punkalaidun","country":"Finland"},
+{"name":"Michael","surname":"Shaw","gender":"Male","email":"mshaw18@webmd.com","city":"Hatu Buh","country":"China"},
+{"name":"John","surname":"Daniels","gender":"Male","email":"jdaniels19@google.nl","postalAddress":"H65","city":"Belmullet","country":"Ireland"},
+{"name":"Roy","surname":"Oliver","gender":"Male","email":"roliver1a@tiny.cc","postalAddress":"46-113","city":"Wilków","country":"Poland"},
+{"name":"Cheryl","surname":"Cooper","gender":"Female","email":"ccooper1b@nhs.uk","city":"Vacoas","country":"Mauritius"},
+{"name":"Russell","surname":"Boyd","gender":"Male","email":"rboyd1c@vk.com","city":"Tumen","country":"China"},
+{"name":"Virginia","surname":"Henry","gender":"Female","email":"vhenry1d@sina.com.cn","city":"Toba","country":"Serbia"},
+{"name":"Amanda","surname":"Warren","gender":"Female","email":"awarren1e@gmpg.org","city":"San Miguel","country":"Peru"},
+{"name":"Bobby","surname":"Kelley","gender":"Male","email":"bkelley1f@bizjournals.com","postalAddress":"606055","city":"Pyra","country":"Russia"},
+{"name":"Frances","surname":"Romero","gender":"Female","email":"fromero1g@mail.ru","city":"Kihurio","country":"Tanzania"},
+{"name":"Scott","surname":"Rivera","gender":"Male","email":"srivera1h@tripod.com","postalAddress":"992-0351","city":"Takahata","country":"Japan"},
+{"name":"Heather","surname":"Sullivan","gender":"Female","email":"hsullivan1i@blogger.com","postalAddress":"403800","city":"Kotovo","country":"Russia"},
+{"name":"Brenda","surname":"Sullivan","gender":"Female","email":"bsullivan1j@theatlantic.com","postalAddress":"1349","city":"København","country":"Denmark"},
+{"name":"Irene","surname":"Hansen","gender":"Female","email":"ihansen1k@hc360.com","city":"Nansan","country":"China"},
+{"name":"Arthur","surname":"Scott","gender":"Male","email":"ascott1l@sphinn.com","postalAddress":"1174","city":"Andop","country":"Philippines"},
+{"name":"Ann","surname":"Martin","gender":"Female","email":"amartin1m@tripadvisor.com","postalAddress":"76711","city":"Waco","country":"United States"},
+{"name":"Kevin","surname":"Matthews","gender":"Male","email":"kmatthews1n@nature.com","city":"Baun","country":"Indonesia"},
+{"name":"Steven","surname":"Sanchez","gender":"Male","email":"ssanchez1o@smugmug.com","city":"Todorovo","country":"Bosnia and Herzegovina"},
+{"name":"Melissa","surname":"King","gender":"Female","email":"mking1p@mozilla.org","city":"Sujiatuo","country":"China"},
+{"name":"Jeremy","surname":"Gibson","gender":"Male","email":"jgibson1q@cdbaby.com","city":"Nueva Arica","country":"Peru"},
+{"name":"Karen","surname":"Cruz","gender":"Female","email":"kcruz1r@cafepress.com","postalAddress":"195047","city":"Florencia","country":"Colombia"},
+{"name":"Marilyn","surname":"Grant","gender":"Female","email":"mgrant1s@nps.gov","postalAddress":"197-0011","city":"Fussa","country":"Japan"},
+{"name":"Adam","surname":"Webb","gender":"Male","email":"awebb1t@networksolutions.com","postalAddress":"35156","city":"San Isidro","country":"Mexico"},
+{"name":"Earl","surname":"Vasquez","gender":"Male","email":"evasquez1u@aol.com","postalAddress":"1144","city":"Batu","country":"Philippines"},
+{"name":"Marie","surname":"Harper","gender":"Female","email":"mharper1v@forbes.com","city":"Urachiche","country":"Venezuela"},
+{"name":"Benjamin","surname":"Olson","gender":"Male","email":"bolson1w@edublogs.org","postalAddress":"259-1335","city":"Hadano","country":"Japan"},
+{"name":"Michael","surname":"Fields","gender":"Male","email":"mfields1x@google.co.jp","postalAddress":"9325-006","city":"Estreito Câmara de Lobos","country":"Portugal"},
+{"name":"Bruce","surname":"Payne","gender":"Male","email":"bpayne1y@cocolog-nifty.com","postalAddress":"6200-723","city":"Tortosendo","country":"Portugal"},
+{"name":"Jessica","surname":"Matthews","gender":"Female","email":"jmatthews1z@walmart.com","city":"Hexing","country":"China"},
+{"name":"Ralph","surname":"Campbell","gender":"Male","email":"rcampbell20@vk.com","postalAddress":"48203","city":"El Cerrito","country":"Mexico"},
+{"name":"Susan","surname":"Alexander","gender":"Female","email":"salexander21@yale.edu","postalAddress":"142390","city":"L’vovskiy","country":"Russia"},
+{"name":"Deborah","surname":"Ramos","gender":"Female","email":"dramos22@digg.com","postalAddress":"32236","city":"Jacksonville","country":"United States"},
+{"name":"Johnny","surname":"Peters","gender":"Male","email":"jpeters23@ameblo.jp","city":"Banyue","country":"China"},
+{"name":"Richard","surname":"Snyder","gender":"Male","email":"rsnyder24@cocolog-nifty.com","postalAddress":"6100","city":"Rufino","country":"Argentina"},
+{"name":"Elizabeth","surname":"Sanchez","gender":"Female","email":"esanchez25@pen.io","postalAddress":"46430-000","city":"Guanambi","country":"Brazil"},
+{"name":"Larry","surname":"Phillips","gender":"Male","email":"lphillips26@microsoft.com","postalAddress":"68721-000","city":"Salinópolis","country":"Brazil"},
+{"name":"Karen","surname":"George","gender":"Female","email":"kgeorge27@earthlink.net","city":"Huaqiao","country":"China"},
+{"name":"Maria","surname":"Carr","gender":"Female","email":"mcarr28@etsy.com","postalAddress":"7900-374","city":"Odivelas","country":"Portugal"},
+{"name":"Andrea","surname":"Armstrong","gender":"Female","email":"aarmstrong29@hp.com","postalAddress":"533 50","city":"Götene","country":"Sweden"},
+{"name":"Carol","surname":"Weaver","gender":"Female","email":"cweaver2a@wufoo.com","postalAddress":"87400-000","city":"Cruzeiro do Oeste","country":"Brazil"},
+{"name":"Louis","surname":"Castillo","gender":"Male","email":"lcastillo2b@house.gov","city":"Kyaka","country":"Tanzania"},
+{"name":"Roger","surname":"Bowman","gender":"Male","email":"rbowman2c@berkeley.edu","postalAddress":"613470","city":"Nema","country":"Russia"},
+{"name":"Betty","surname":"Evans","gender":"Female","email":"bevans2d@nhs.uk","city":"Jiangti","country":"China"},
+{"name":"Roy","surname":"Stewart","gender":"Male","email":"rstewart2e@smh.com.au","city":"Qiulu","country":"China"},
+{"name":"Anne","surname":"Harrison","gender":"Female","email":"aharrison2f@walmart.com","postalAddress":"78900-000","city":"Porto Velho","country":"Brazil"},
+{"name":"Antonio","surname":"Sims","gender":"Male","email":"asims2g@yandex.ru","city":"Awayan","country":"Indonesia"},
+{"name":"Jack","surname":"Bradley","gender":"Male","email":"jbradley2h@mapquest.com","city":"Jingouhe","country":"China"},
+{"name":"Benjamin","surname":"Washington","gender":"Male","email":"bwashington2i@icq.com","postalAddress":"3502","city":"Utrecht (stad)","country":"Netherlands"},
+{"name":"Margaret","surname":"Mitchell","gender":"Female","email":"mmitchell2j@webnode.com","city":"Bình Thủy","country":"Vietnam"},
+{"name":"Louise","surname":"Banks","gender":"Female","email":"lbanks2k@sun.com","postalAddress":"745 38","city":"Enköping","country":"Sweden"},
+{"name":"Irene","surname":"Harris","gender":"Female","email":"iharris2l@livejournal.com","city":"Puncakwangi","country":"Indonesia"},
+{"name":"Raymond","surname":"Walker","gender":"Male","email":"rwalker2m@unblog.fr","postalAddress":"05-092","city":"Łomianki","country":"Poland"},
+{"name":"Carolyn","surname":"Evans","gender":"Female","email":"cevans2n@taobao.com","city":"Sharm el-Sheikh","country":"Egypt"},
+{"name":"Judy","surname":"Harvey","gender":"Female","email":"jharvey2o@sina.com.cn","city":"Daqiao","country":"China"},
+{"name":"Anne","surname":"Stewart","gender":"Female","email":"astewart2p@blogtalkradio.com","city":"Namwala","country":"Zambia"},
+{"name":"John","surname":"Wood","gender":"Male","email":"jwood2q@nasa.gov","postalAddress":"567 24","city":"Vaggeryd","country":"Sweden"},
+{"name":"Jack","surname":"Graham","gender":"Male","email":"jgraham2r@mit.edu","city":"Dongdai","country":"China"}]
+
+var getData = function() {
+    var text = "";
+    for(i = 0; i < people.length; i++) {
+        // text += "<tr><td>" + people[i].name + "</td><td>" + people[i].surname + "</td><td><button type=\"button\" class=\"btn btn-info\">Info</button></td></tr>";
+text += "<tr><td>" + people[i].name + "</td><td>" + people[i].surname + "</td><td><button type='button' class='btn btn-info' data-toggle='modal' data-target='#myModal' onclick='currentIndex(" + i + ")'>Info</button></td></tr>";
+    }
+    document.getElementById("peopleBody").innerHTML = text;
+
+}
+
+var currentIndex = function(index) {
+    document.getElementById("name").innerHTML = people[index].name;
+    document.getElementById("surname").innerHTML = people[index].surname;
+    document.getElementById("gender").innerHTML = people[index].gender;
+    document.getElementById("email").innerHTML = people[index].email;
+    document.getElementById("city").innerHTML = people[index].city;
+    document.getElementById("country").innerHTML = people[index].country;
+
+    document.getElementById("name1").innerHTML = people[index].name;
+    document.getElementById("surname1").innerHTML = people[index].surname;
+    document.getElementById("gender1").innerHTML = people[index].gender;
+    document.getElementById("email1").innerHTML = people[index].email;
+    document.getElementById("city1").innerHTML = people[index].city;
+    document.getElementById("country1").innerHTML = people[index].country;
+
+
+}
